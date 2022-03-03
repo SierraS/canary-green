ad69445b7cbfa42bd8e6ae62f082561656533e2a
SierraS <schellenbergsierra@gmail.com>
2022-03-02 23:43:07 -0500

    Add vpc, SG, subnet, and subnet role

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,14 @@
-import { App, Stack, StackProps, RemovalPolicy, CfnOutput, Duration } from "aws-cdk-lib";
+import {
+  App,
+  Stack,
+  StackProps,
+  RemovalPolicy,
+  CfnOutput,
+  Duration,
+} from "aws-cdk-lib";
 import { aws_s3 as s3 } from "aws-cdk-lib";
+import { aws_ec2 as ec2 } from "aws-cdk-lib";
+import { aws_iam as iam } from "aws-cdk-lib";
 // https://docs.aws.amazon.com/cdk/v2/guide/migrating-v2.html
 import * as synthetics from "@aws-cdk/aws-synthetics-alpha";
 import path = require("path");
@@ -23,11 +32,11 @@ export class AppStack extends Stack {
       value: bucket.bucketName,
     });
 
-    const canary = new synthetics.Canary(this, 'MyCanary', {
+    const canary = new synthetics.Canary(this, "MyCanary", {
       schedule: synthetics.Schedule.rate(Duration.minutes(50)),
       test: synthetics.Test.custom({
         code: synthetics.Code.fromInline(
-`const synthetics = require("Synthetics");
+          `const synthetics = require("Synthetics");
 const log = require("SyntheticsLogger");
 
 const pageLoadBlueprint = async function () {
@@ -53,15 +62,39 @@ const pageLoadBlueprint = async function () {
 exports.handler = async () => {
   return await pageLoadBlueprint();
 };`
-
-
         ),
-        handler: 'index.handler',
+        handler: "index.handler",
       }),
       runtime: synthetics.Runtime.SYNTHETICS_NODEJS_PUPPETEER_3_1,
       environmentVariables: {
-        stage: 'prod',
+        stage: "prod",
       },
     });
+
+    const vpc1 = new ec2.Vpc(this, "VPC1");
+
+    // security group
+    const sg1 = new ec2.SecurityGroup(this, "SG1", { vpc: vpc1 });
+
+    // subnet
+    const subnet = new ec2.Subnet(this, "subnet", {
+      availabilityZone: "us-east-1a",
+      cidrBlock: "10.0.0.0/24",
+      vpcId: vpc1.vpcId,
+    });
+
+    // role1: role with subnet permissions
+    const role1 = new iam.Role(this, "roleWithSubnetOnly", {
+      assumedBy: new iam.AccountPrincipal(this.account),
+    });
+    role1.addToPolicy(
+      new iam.PolicyStatement({
+        actions: ["ec2:CreateNetworkInterface"],
+        resources: [
+          `arn:${this.partition}:ec2:${this.region}:${this.account}:subnet/${subnet.subnetId}`,
+        ],
+      })
+    );
+    // role 2: role with networkinterface and subnet permissions
   }
 }
